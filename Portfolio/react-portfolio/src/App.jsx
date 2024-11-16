7cae59957b861ce5e6265f61c3b90f01f8758479
Spruha-U <spruhaukidave@gmail.com>
2024-11-16 15:22:31 +0100

    updated syntax and added closeIcon

--- Portfolio/react-portfolio/src/App.jsx
+++ Portfolio/react-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 
-import styles from "./App.module.css"
-import { Navbar } from "./components/Navbar/Navbar"
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
@@ -11,4 +11,4 @@ function App() {
     
   }
 
-export default App
+export default App;
